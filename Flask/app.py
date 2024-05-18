b18976c014002052a9d89a965e9035c0249efbd9
sriramk146 <57525668+sriramk146@users.noreply.github.com>
2024-05-18 14:07:23 +0530

    First commit practice for flask

---
+++ Flask/app.py
@@ -0,0 +1,34 @@
+### Building Url Dynamically 
+####Variable Rules And URL Building
+
+from flask import Flask,redirect,url_for
+import pandas
+app=Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return 'Welcome to my Youtube Channel'
+
+@app.route('/success/<int:score>')
+def success(score):
+    return "The Person has Passed and the marks is "+ str(score)
+
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return "The Person has failed and the marks is "+ str(score)
+
+### Result checker
+@app.route('/results/<int:marks>')
+def results(marks):
+    result=""
+    if marks<50:
+        result='fail'
+    else:
+        result='success'
+    return redirect(url_for(result,score=marks))
+
+
+if __name__=='__main__':
+    app.run(debug=True)
+ 
